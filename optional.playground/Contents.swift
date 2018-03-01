0f7df14dab67f8a3a17a764f0d28608c8c6939a5
West Kraemer <kraemerwest@gmail.com>
2018-02-28 19:50:35 -0500

    more work with options

--- optional.playground/Contents.swift
+++ optional.playground/Contents.swift
@@ -2,8 +2,20 @@
 
 import UIKit
 
+var result: Int? = 30
+print(result)
+
+//force unwrap
+print(result! + 1)
 
 
+var authorName: String? = nil
+var authorAge: String? = "32"
 
-
+//optional binding (if there is a value, do something with it...)
+if let unwrappedAuthorName = authorName {
+    print("Author is \(unwrappedAuthorName)")
+} else {
+    print("No author")
+}
 
